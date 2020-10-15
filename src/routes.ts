d32c1c6fc91769aee55aa61e8ee5ed7308db41d0
Lucas <lucas.silveira@elevenfinancial.com>
2020-10-15 17:07:25 -0300

    Level #2 DONE!!!

--- src/routes.ts
+++ src/routes.ts
@@ -1,10 +1,16 @@
 import {Router} from 'express';
+import multer from 'multer';
 
+import uploadConfig from './config/upload'
 import OrphanagesController from './controllers/OrphanagesController'
+
+const upload = multer(uploadConfig)
 
 
 const routes = Router();
 
-routes.post("/orphanages", OrphanagesController.create);
+routes.post("/orphanages", upload.array('images') ,OrphanagesController.create);
+routes.get("/orphanages", OrphanagesController.index);
+routes.get("/orphanages/:id", OrphanagesController.show);
 
 export default routes;
